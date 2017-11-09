3f73098aa170221e47ec2bbd13e36edde8cdca9c
suyongyao <33447511+suyongyao@users.noreply.github.com>
2017-11-09 10:09:05 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,68 @@
+//something to test
+const op1 = (width, x) => {
+    let result = width + x;
+    if (result < 0 ) {
+        result = 0;
+    }
+    return result;
+}
+
+// Tiny unit test framework
+const test = (component, fn, count = 1) => {
+    console.log(`# ${ component }`);
+    fn({
+      same: (actual, expected, msg) => {
+        if (actual == expected) {
+          console.log(`ok ${ count } - ${ msg }`);
+        } else {
+          throw new Error(
+      `not ok ${ count } -  ${ msg }
+        expected:
+          ${ expected }
+        actual:
+          ${ actual }
+      `
+          );
+        }
+        count++;
+      }
+    });
+  };
+
+
+// Test suite for op1
+test('op1', assert => {
+    {
+        const msg = 'op1() add positive number';
+
+        const actual = op1(0, 10);
+        const expected = 10;
+
+        assert.same(actual, expected, msg);
+    }
+    {
+        const msg = 'op1() negative result should return 0';
+
+        const actual = op1(0, -10);
+        const expected = 0;
+
+        assert.same(actual, expected, msg);
+    }
+    {
+        const msg = 'op1() add positive number';
+
+        const actual = op1(10, 10);
+        const expected = 20;
+
+        assert.same(actual, expected, msg);
+    }
+    {
+        const msg = 'op1() add negative number';
+
+        const actual = op1(70, -10);
+        const expected = 60;
+
+        assert.same(actual, expected, msg);
+    }
+
+})
