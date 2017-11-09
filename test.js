38723e725791df1c27d06f9f49832e2fa5f90a8c
suyongyao <33447511+suyongyao@users.noreply.github.com>
2017-11-09 11:43:55 +0800

    Add files via upload

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 //something to test
 const op1 = (width, x) => {
+    x = +x;
+    width = +width;
     let result = width + x;
     if (result < 0 ) {
         result = 0;
@@ -33,7 +35,7 @@ const test = (component, fn, count = 1) => {
 // Test suite for op1
 test('op1', assert => {
     {
-        const msg = 'op1() add positive number';
+        const msg = 'op1() add positive number.';
 
         const actual = op1(0, 10);
         const expected = 10;
@@ -41,7 +43,7 @@ test('op1', assert => {
         assert.same(actual, expected, msg);
     }
     {
-        const msg = 'op1() negative result should return 0';
+        const msg = 'op1() negative result should return 0.';
 
         const actual = op1(0, -10);
         const expected = 0;
@@ -49,17 +51,33 @@ test('op1', assert => {
         assert.same(actual, expected, msg);
     }
     {
-        const msg = 'op1() add positive number';
+        const msg = 'op1() add number and +string.';
 
-        const actual = op1(10, 10);
+        const actual = op1(10, "+10");
         const expected = 20;
 
         assert.same(actual, expected, msg);
     }
     {
-        const msg = 'op1() add negative number';
+        const msg = 'op1() add number and -string.';
 
-        const actual = op1(70, -10);
+        const actual = op1(70, "-10");
+        const expected = 60;
+
+        assert.same(actual, expected, msg);
+    }
+    {
+        const msg = 'op1() add +string and number.';
+
+        const actual = op1("+10", 10);
+        const expected = 20;
+
+        assert.same(actual, expected, msg);
+    }
+    {
+        const msg = 'op1() add -string and number.';
+
+        const actual = op1("-10", 70);
         const expected = 60;
 
         assert.same(actual, expected, msg);
